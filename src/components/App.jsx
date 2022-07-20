de50821f8594bf0b75ec537024c433515479f13b
Yuriy Pokhyl <woully@gmail.com>
2022-07-20 22:32:48 +0300

    state machine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    pokemon: null,
+
+    error: null,
+    status: 'idle',
+  };
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevProps.pokemonName;
+    const nextName = this.props.pokemonName;
+
+    if (prevName !== nextName) {
+      this.state({ status: 'pending' });
+
+      fetch(`https://pkeapi.co/api/v2/pokemon/${nextName}`)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(new Error('No pokemon '));
+        })
+        .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
+        .catch(error => this.setState({ error, status: 'rejected' }));
+    }
+  }
+
+  render() {
+    const { pokemon, error, status } = this.state;
+
+    if (status === 'idle') {
+      return <div>Enter pokemon Name</div>;
+    }
+
+    if (status === 'pending') {
+      return <div>Loading..</div>;
+    }
+
+    if (status === 'rejected') {
+      return <h1>{error.message}</h1>;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <div>
+          <p>{pokemon.name}</p>
+          <img
+            src={pokemon.sprites.other['official-artwork'].front_default}
+            alt={pokemon.name}
+            width="240"
+          />
+        </div>
+      );
+    }
+  }
+}
